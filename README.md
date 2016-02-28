740187d1f41d45620e77a1d78c5ae1e219ddcbbb
malinatran <tran.malina@gmail.com>
2016-02-27 23:38:29 -0500

    Completed game

---
+++ README.md
@@ -0,0 +1,15 @@
+## Tic Tac Toe
+Malina Tran
+
+### Objectives
+* Build a simple tic tac toe game that allows for a human player
+* Ensure that the AI never loses and wins whenever possible
+* Create a user interface
+
+### Approach
+Upon identifying the basic strategies of tic tac toe, I set up the game to allow the AI to always start first, select the center square, and subsequently select one of the four corners of board game. The game logic is as follows: the AI's goals are to identify opportunities for a win (three in a row), to block the human player from achieving a possible win, and to make a strategic move based on the game's progression.
+
+### Future Improvements
+* Given the AI's comparative advantages in this current version, I would enable this game to switch between the computer and player starting first.
+* Rather than randomize remaining squares (if only three squares are remaining), I would employ a smarter selection based on neighboring squares and potential win. 
+* Other ideas for improving the game include: allowing a two-player version, including a scoreboard, and also incorporating rounds for multiple plays and an overall winner.
--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-console.log('yo');
-
-// Tic Tac Toe Universe
+// The Universe
 var human = {
-  name: 'human',
-  points: 0,
-  wins: 0
+  name: 'human'
 };
 
 var computer = {
-  name: 'computer',
-  points: 0,
-  wins: 0
+  name: 'computer'
 };
 
 var board = [
@@ -19,312 +13,246 @@ var board = [
   [null, null, null]
 ];
 
-var boardMapping = {
-  'one': 'board[0][0]',
-  'two': 'board[0][1]',
-  'three': 'board[0][2]',
-  'four': 'board[1][0]',
-  'five': 'board[1][1]',
-  'six': 'board[1][2]',
-  'seven': 'board[2][0]',
-  'eight': 'board[2][1]',
-  'nine': 'board[2][2]'
-};
-
-// var gameOver = false;
-
 // Functions
 function startGame() {
-  // Welcome message or something
-  // Disable start button
-  // Reset board
-  $('#five').addClass('selected').text('X').unbind('click');
-  board[1][1] = computer.name;
-  console.log('game started');
-};
+  // Computer goes first and always selects center
+  makeCompMove(1, 1);
+  $('.square').on('click', function() {
+    var coordinates = $(this).data('grid-id').split('-');
+    var x = coordinates[0];
+    var y = coordinates[1];
+    makeHumanMove(x, y);
+  });
+}
 
-function makeHumanMove(x, y, element) {
+function getSquare(x, y) {
+  // Identifies selected square based on data attribute
+  var filter = '[data-grid-id="' + x + '-' + y + '"]';
+  return $('.square' + filter);
+}
+
+function makeHumanMove(x, y) {
   if (board[x][y] === null) {
-    $(element).addClass('selected').text('O');
+    var $square = getSquare(x, y);
+    $square.addClass('selected').text('O');
     board[x][y] = human.name;
+    determineMove();
     checkForWinner();
   }
-};
+}
 
 function makeCompMove(x, y) {
-  for (var key in boardMapping) {
-    if (boardMapping[key] === 'board[' + x + '][' + y + ']') {
-      var element = '#' + key;
-      $(element).addClass('selected').text('X');
-    }
-  }
-  // checkForWinner();
-};
+  var $square = getSquare(x, y);
+  $square.addClass('selected').text('X');
+  board[x][y] = computer.name;
 
-// Before blocking & making a move, checks to see if player has won (checking 3 moves).
+  var selectedNum = $('.selected').length;
+  if (selectedNum === 9) {
+    announceWinner();
+  } 
+}
+
 function checkForWinner() {
-  // console.log('checking for winner');
   if ((board[0][0] === 'human' && board[0][0] === board[0][1] && board[0][0] === board[0][2]) || 
-   (board[1][0] === 'human' && board[1][0] === board[1][1] && board[1][0] === board[1][2]) ||
-   (board[2][0] === 'human' && board[2][0] === board[2][1] && board[2][0] === board[2][2]) ||
-   (board[0][0] === 'human' && board[0][0] === board[1][0] && board[0][0] === board[2][0]) ||
-   (board[0][1] === 'human' && board[0][1] === board[1][1] && board[0][1] === board[2][1]) ||
-   (board[0][2] === 'human' && board[0][2] === board[1][2] && board[0][2] === board[2][2]) ||
-   (board[0][0] === 'human' && board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
-   (board[2][0] === 'human' && board[2][0] === board[1][1] && board[2][0] === board[0][2]) ||
-   (board[0][0] === 'computer' && board[0][0] === board[0][1] && board[0][0] === board[0][2]) || 
-   (board[1][0] === 'computer' && board[1][0] === board[1][1] && board[1][0] === board[1][2]) ||
-   (board[2][0] === 'computer' && board[2][0] === board[2][1] && board[2][0] === board[2][2]) ||
-   (board[0][0] === 'computer' && board[0][0] === board[1][0] && board[0][0] === board[2][0]) ||
-   (board[0][1] === 'computer' && board[0][1] === board[1][1] && board[0][1] === board[2][1]) ||
-   (board[0][2] === 'computer' && board[0][2] === board[1][2] && board[0][2] === board[2][2]) ||
-   (board[0][0] === 'computer' && board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
-   (board[2][0] === 'computer' && board[2][0] === board[1][1] && board[2][0] === board[0][2])) {
-    console.log('updating score');
-    updateScore();
-  } else {
-    determineMove();
+      (board[1][0] === 'human' && board[1][0] === board[1][1] && board[1][0] === board[1][2]) ||
+      (board[2][0] === 'human' && board[2][0] === board[2][1] && board[2][0] === board[2][2]) ||
+      (board[0][0] === 'human' && board[0][0] === board[1][0] && board[0][0] === board[2][0]) ||
+      (board[0][1] === 'human' && board[0][1] === board[1][1] && board[0][1] === board[2][1]) ||
+      (board[0][2] === 'human' && board[0][2] === board[1][2] && board[0][2] === board[2][2]) ||
+      (board[0][0] === 'human' && board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
+      (board[2][0] === 'human' && board[2][0] === board[1][1] && board[2][0] === board[0][2])) {
+    announceWinner('human');
+  } else if
+      ((board[0][0] === 'computer' && board[0][0] === board[0][1] && board[0][0] === board[0][2]) || 
+      (board[1][0] === 'computer' && board[1][0] === board[1][1] && board[1][0] === board[1][2]) ||
+      (board[2][0] === 'computer' && board[2][0] === board[2][1] && board[2][0] === board[2][2]) ||
+      (board[0][0] === 'computer' && board[0][0] === board[1][0] && board[0][0] === board[2][0]) ||
+      (board[0][1] === 'computer' && board[0][1] === board[1][1] && board[0][1] === board[2][1]) ||
+      (board[0][2] === 'computer' && board[0][2] === board[1][2] && board[0][2] === board[2][2]) ||
+      (board[0][0] === 'computer' && board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
+      (board[2][0] === 'computer' && board[2][0] === board[1][1] && board[2][0] === board[0][2])) {
+    announceWinner('computer');
   }
-};
+}
 
 function determineMove() {
+  // Ensures a corner is selected for computer's 2nd turn
   var selectedNum = $('.selected').length;
   if (selectedNum === 2) {
     makeCornerMove();
-  } else if (selectedNum === 4) {
-    console.log('determining move');
-    makeWinningMove();
-  } else if (selectedNum >= 6) {
-    checkRemainingMoves();
   } else {
-    console.log('someting');
-    blockMove();
+    makeWinningMove();
   }
-};
+}
 
 function makeWinningMove() {
-  console.log('make winning move');
-  // Checking through horizontal & vertical possibilities
-  for (var i = 0; i < 3; i++) {
+  // Checks horizontal & vertical possibilities for a chance to block human player
+  for (var i = 0; i < board.length; i++) {
     if (board[i][0] === board[i][1] && board[i][0] === 'computer') {
       if (board[i][2] === null) {
-        board[i][2] = computer.name;
         makeCompMove(i, 2);
         return;
       } 
     } else if (board[i][0] === board[i][2] && board[i][0] === 'computer') {
       if (board[i][1] === null) {
-        board[i][1] = computer.name;
         makeCompMove(i, 1);
         return;
       } 
     } else if (board[i][1] === board[i][2] && board[i][1] === 'computer') {
       if (board[i][0] === null) {
-        board[i][0] = computer.name;
         makeCompMove(i, 0);
         return;
       } 
     } else if (board[0][i] === board[1][i] && board[0][i] === 'computer') {
       if (board[2][i] === null) {
-        board[2][i] = computer.name;
         makeCompMove(2, i);
         return;
       } 
     } else if (board[0][i] === board[2][i] && board[0][i] === 'computer') {
       if (board[1][i] === null) {
-        board[1][i] = computer.name;
         makeCompMove(1, i);
         return;
       } 
     } else if (board[1][i] === board[2][i] && board[1][i] === 'computer') {
       if (board[0][i] === null) {
-        board[0][i] = computer.name;
         makeCompMove(0, i);
         return;
       } 
     }
   }
-  // Checking through diagonal possibilities
+  // Checks diagonal possibilities for a chance to win
   if (board[0][0] === board[1][1] && board[0][0] === 'computer' && board[2][2] === null) {
-    board[2][2] = computer.name;
     makeCompMove(2, 2);
   } else if (board[0][0] === board[2][2] && board[0][0] === 'computer' && board[1][1] === null) {
-    board[1][1] = computer.name;
     makeCompMove(1, 1);
   } else if (board[1][1] === board[2][2] && board[1][1] === 'computer' && board[0][0] === null) {
-    board[0][0] = computer.name;
     makeCompMove(0, 0);
   } else if (board[0][2] === board[1][1] && board[0][2] === 'computer' && board[2][0] === null) {
-    board[2][0] = computer.name;
     makeCompMove(2, 0);
   } else if (board[0][2] === board[2][0] && board[0][2] === 'computer' && board[1][1] === null) {
-    board[1][1] = computer.name;
     makeCompMove(1, 1);
   } else if (board[1][1] === board[2][0] && board[1][1] === 'computer' && board[0][2] === null) {
-    board[0][2] = computer.name;
     makeCompMove(0, 2);
   } else {
-    blockMove();
+    if ($('.selected').length >= 6) {
+      checkRemainingMoves();
+    } else {
+      blockMove();
+    }
   }
-};
+}
 
-// Before making a move, checks to see if player is about to win and BLOCK THE HUMAN (checking 2 moves).
 function blockMove() {
-  // Checking through horizontal & vertical possibilities
-  console.log('here');
-  for (var i = 0; i < 3; i++) {
+  // Checks horizontal & vertical possibilities for a chance to block human player
+  for (var i = 0; i < board.length; i++) {
     if (board[i][0] === board[i][1] && board[i][0] === 'human') {
-      board[i][2] = computer.name;
       makeCompMove(i, 2);
       return;
     } else if (board[i][0] === board[i][2] && board[i][0] === 'human') {
-      board[i][1] = computer.name;
       makeCompMove(i, 1);
       return;
     } else if (board[i][1] === board[i][2] && board[i][1] === 'human') {
-      board[i][0] = computer.name;
       makeCompMove(i, 0);
       return;
     } else if (board[0][i] === board[1][i] && board[0][i] === 'human') {
-      board[2][i] = computer.name;
       makeCompMove(2, i);
       return;
     } else if (board[0][i] === board[2][i] && board[0][i] === 'human') {
-      board[1][i] = computer.name;
       makeCompMove(1, i);
       return;
     } else if (board[1][i] === board[2][i] && board[1][i] === 'human') {
-      board[0][i] = computer.name;
       makeCompMove(0, i);
       return;
     }
   }
-  // Checking through diagonal possibilities
+  // Checks diagonal possibilities for a chance to block human player
   if (board[0][0] === board[1][1] && board[0][0] === 'human') {
-    board[2][2] = computer.name;
     makeCompMove(2, 2);
   } else if (board[0][0] === board[2][2] && board[0][0] === 'human') {
-    board[1][1] = computer.name;
     makeCompMove(1, 1);
   } else if (board[1][1] === board[2][2] && board[1][1] === 'human') {
-    board[0][0] = computer.name;
     makeCompMove(0, 0);
   } else if (board[0][2] === board[1][1] && board[0][2] === 'human') {
-    board[2][0] = computer.name;
     makeCompMove(2, 0);
   } else if (board[0][2] === board[2][0] && board[0][2] === 'human') {
-    board[1][1] = computer.name;
     makeCompMove(1, 1);
   } else if (board[1][1] === board[2][0] && board[1][1] === 'human') {
-    board[0][2] = computer.name;
     makeCompMove(0, 2);
   } else {
-    checkForWinner();
+    checkRemainingMoves();
   }
-};
+}
 
-function makeCornerMove() { 
-  if (board[0][0] === null) {
-    $('#one').addClass('selected').text('X');
-    board[0][0] = computer.name;
-  } else if (board[0][2] === null) {
-    $('#three').addClass('selected').text('X');
-    board[0][2] = computer.name;
-  } else if (board[2][0] === null) {
-    $('#seven').addClass('selected').text('X');
-    board[2][0] = computer.name; 
-  } else if (board[2][2] === null) {
-    $('#nine').addClass('selected').text('X');
-    board[2][2] = computer.name;
-  }
-};
+function makeCornerMove() {
+  // Randomly selects one of the four corners
+  var corners = $.grep([
+    [0, 0],
+    [0, 2],
+    [2, 0],
+    [2, 2]
+  ], function(coordinates) {
+    var x = coordinates[0];
+    var y = coordinates[1];
+    return board[x][y] === null;
+  });
+  var randomCoordinates = corners[Math.floor(Math.random()*corners.length)];
+  var x = randomCoordinates[0];
+  var y = randomCoordinates[1];
+  makeCompMove(x, y);
+
+}
 
 function checkRemainingMoves() {
+  // If no chancee at winning or blocking, then randomly selects one of the remaining squares
   var arr = [];
-  var position = { 'x': null, 'y': null };
   for (var i = 0; i < board.length; i++) {
-    var row = board[i];
-    for (var j = 0; j < row.length; j++) {
+    for (var j = 0; j < board[i].length; j++) {
       if (board[i][j] === null) {
-        position.x = i;
-        position.y = j;
-        arr.push(position);
+        arr.push({
+          x: i,
+          y: j
+        });
       }
     }
   }
   var randNum = Math.floor(Math.random() * arr.length - 1) + 1;
   var x = arr[randNum].x;
   var y = arr[randNum].y;
-  board[x][y] = computer.name;
   makeCompMove(x, y);
-};
-
-function updateScore() {
-  alert('WINNER');
 }
 
-// function determineRoundWinner()
+function announceWinner(winner) {
+  $('.square').off('click');
+  if (winner === 'computer') {
+    $('#message-board').text('Sorry, you lose!');
+  } else if (winner === 'human') {
+    $('#message-board').text('Congrats, you win – somehow!');
+  } else {
+    $('#message-board').text('It\'s a tie!');
+  }
+}
 
-// function determineOverallWinner()
-// Calculate after each round and see if 5 wins by player or computer
-
-// function resetBoard()
+function clearBoard() {
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      board[i][j] = null;
+    }
+  }
+  $('.square').off('click').removeClass('selected').text('');
+  $('#message-board').text('');
+}
 
 $(function() {
   $('#start-button').on('click', function() {
+    $('#reset-button').show();
+    $('#start-button').hide();
     startGame();
   });
 
-  // for (var i = 0; i < 9; i++) {
-  //   $('.square').on('click', function() {
-  //     var coordinates = $(this).data('grid-id').split('-');
-  //     makeHumanMove(coordinates);
-  //   });
-  // };
-
-  var $one = $('#one');
-  $one.on('click', function() {
-    makeHumanMove(0, 0, one);
-  });
-
-  var $two = $('#two');
-  $two.on('click', function() {
-    makeHumanMove(0, 1, two);
-  });
-
-  var $three = $('#three');
-  $three.on('click', function() {
-    makeHumanMove(0, 2, three);
-  });
-
-  var $four = $('#four');
-  $four.on('click', function() {
-    makeHumanMove(1, 0, four);
-  });
-
-  var $five = $('#five');
-  $five.on('click', function() {
-    makeHumanMove(1, 1, five);
-  });
-
-  var $six = $('#six');
-  $six.on('click', function() {
-    makeHumanMove(1, 2, six);
-  });
-
-  var $seven = $('#seven');
-  $seven.on('click', function() {
-    makeHumanMove(2, 0, seven);
-  });
-
-  var $eight = $('#eight');
-  $eight.on('click', function() {
-    makeHumanMove(2, 1, eight);
-  });
-
-  var $nine = $('#nine');
-  $nine.on('click', function() {
-    makeHumanMove(2, 2, nine);
+  $('#reset-button').on('click', function() {
+    $('#reset-button').hide();
+    $('#start-button').show();
+    clearBoard();
   });
 
 });
